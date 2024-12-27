d100ff413a7185daa1f4d991caade16a33fd8f6b
hantschmann <anton.hantschmann@campus.lmu.de>
2024-12-27 12:25:11 +0100

    added statisticsRepo helper methods

--- lib/repositories/statistics_repository.dart
+++ lib/repositories/statistics_repository.dart
@@ -32,4 +32,37 @@ class StatisticsRepository {
     _saveStatistics()
     resetStatistics()??
   */
+
+  /// add new attempt to existing statistic
+  Future<void> addAttempt(GameAttempt attempt) async {
+    try {
+      // get copy of list recentAttempts and add new attempt
+      final updatedAttempts =
+          List<GameAttempt>.from(_currentStats.recentAttempts);
+      updatedAttempts.add(attempt);
+
+      _currentStats = _currentStats.copyWith(
+        totalAttempts: _currentStats.totalAttempts + 1,
+        correctGuesses:
+            _currentStats.correctGuesses + (attempt.wasCorrect ? 1 : 0),
+        recentAttempts: updatedAttempts,
+      );
+      await _saveStatistics();
+    } catch (e) {
+      throw Exception("Failed to add attempt: $e");
+    }
+  }
+
+  Future<void> _saveStatistics() async {
+    //todo: implement database or json
+  }
+
+  Future<void> resetStatistics() async {
+    try {
+      _currentStats = Statistics.initial();
+      await _saveStatistics();
+    } catch (e) {
+      throw Exception("Failed to reset statistics $e");
+    }
+  }
 }
