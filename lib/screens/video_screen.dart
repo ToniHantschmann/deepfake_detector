0ed1eab96cba251e8fa5a7b6d5d0e3f8734d1ad5
hantschmann <anton.hantschmann@campus.lmu.de>
2025-01-02 16:25:07 +0100

    changed video_screen implementation again

--- lib/screens/video_screen.dart
+++ lib/screens/video_screen.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:html' as html;
-import 'dart:ui' as ui;
-import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:video_player/video_player.dart';
 import '../models/video_model.dart';
 
@@ -9,9 +6,6 @@ class VideoScreen extends StatefulWidget {
   final Video video;
   final VoidCallback onNext;
   final bool isFirstVideo;
-
-  //final VoidCallback onNext;
-  // TODO: get video from GameState
 
   const VideoScreen({
     Key? key,
@@ -25,112 +19,31 @@ class VideoScreen extends StatefulWidget {
 }
 
 class VideoScreenState extends State<VideoScreen> {
-  VideoPlayerController? _mobileController;
-  html.VideoElement? _webVideoElement;
-  final String _webVideoPlayerId =
-      'video-player-${DateTime.now().millisecondsSinceEpoch}';
-  bool _isPlaying = false;
-  double _progress = 0.0;
+  late VideoPlayerController _controller;
+  bool _isInitialized = false;
 
   @override
   void initState() {
     super.initState();
-    if (kIsWeb) {
-      _initializeWebVideo();
-    } else {
-      _initializeMobileVideo();
-    }
+    _initializeVideo();
   }
 
-  void _initializeWebVideo() {
-    // HTML Video Element erstellen
-    _webVideoElement = html.VideoElement()
-      ..id = _webVideoPlayerId
-      ..src =
-          'assets/${widget.video.videoUrl}' // Video muss in web/assets liegen
-      ..style.width = '100%'
-      ..style.height = '100%'
-      ..style.objectFit = 'contain';
+  Future<void> _initializeVideo() async {
+    _controller = VideoPlayerController.asset(widget.video.videoUrl);
 
-    // Controls hinzufügen
-    _webVideoElement!
-      ..controls = false
-      ..onTimeUpdate.listen((_) {
-        if (mounted) {
-          setState(() {
-            _progress =
-                _webVideoElement!.currentTime / _webVideoElement!.duration;
-          });
-        }
-      })
-      ..onPlay.listen((_) {
-        if (mounted) setState(() => _isPlaying = true);
-      })
-      ..onPause.listen((_) {
-        if (mounted) setState(() => _isPlaying = false);
-      });
-
-    // Video Element in den DOM einfügen
-    // ignore: undefined_prefixed_name
-    ui.platformViewRegistry.registerViewFactory(
-      _webVideoPlayerId,
-      (int viewId) => _webVideoElement!,
-    );
-  }
-
-  Future<void> _initializeMobileVideo() async {
-    _mobileController = VideoPlayerController.asset(widget.video.videoUrl);
-
-    await _mobileController!.initialize();
-    _mobileController!.addListener(() {
-      if (!mounted) return;
+    try {
+      await _controller.initialize();
       setState(() {
-        _isPlaying = _mobileController!.value.isPlaying;
-        if (_mobileController!.value.duration.inMilliseconds > 0) {
-          _progress = _mobileController!.value.position.inMilliseconds /
-              _mobileController!.value.duration.inMilliseconds;
-        }
+        _isInitialized = true;
       });
-    });
-    setState(() {});
-  }
-
-  void _togglePlayPause() {
-    setState(() {
-      if (kIsWeb) {
-        if (_isPlaying) {
-          _webVideoElement!.pause();
-        } else {
-          _webVideoElement!.play();
-        }
-      } else {
-        if (_mobileController!.value.isPlaying) {
-          _mobileController!.pause();
-        } else {
-          _mobileController!.play();
-        }
-      }
-    });
-  }
-
-  void _replay() {
-    if (kIsWeb) {
-      _webVideoElement!.currentTime = 0;
-      _webVideoElement!.play();
-    } else {
-      _mobileController!.seekTo(Duration.zero);
-      _mobileController!.play();
+    } catch (e) {
+      debugPrint('Error initializing video: $e');
     }
-    setState(() => _isPlaying = true);
   }
 
   @override
   void dispose() {
-    if (kIsWeb) {
-      _webVideoElement?.remove();
-    } else {
-      _mobileController?.dispose();
-    }
+    _controller.dispose();
     super.dispose();
   }
 
@@ -166,71 +79,80 @@ class VideoScreenState extends State<VideoScreen> {
               ),
             ),
 
-            // Video Player mit Fortschrittsanzeige
+            // Video Player und Fortschrittsanzeige
             Center(
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: [
                   AspectRatio(
                     aspectRatio: 16 / 9,
-                    child: kIsWeb
-                        ? HtmlElementView(viewType: _webVideoPlayerId)
-                        : _mobileController?.value.isInitialized ?? false
-                            ? VideoPlayer(_mobileController!)
-                            : const Center(child: CircularProgressIndicator()),
+                    child: _isInitialized
+                        ? VideoPlayer(_controller)
+                        : const Center(
+                            child: CircularProgressIndicator(),
+                          ),
                   ),
-                  // Video Progress Bar
-                  Container(
-                    width: MediaQuery.of(context).size.width * 0.8,
-                    height: 4,
-                    margin: const EdgeInsets.only(top: 8),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(2),
-                      color: Colors.grey[800],
-                    ),
-                    child: FractionallySizedBox(
-                      alignment: Alignment.centerLeft,
-                      widthFactor: _progress,
-                      child: Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(2),
-                          color: Colors.blue,
+                  if (_isInitialized)
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      margin: const EdgeInsets.only(top: 8),
+                      child: VideoProgressIndicator(
+                        _controller,
+                        allowScrubbing: true,
+                        colors: VideoProgressColors(
+                          playedColor: Colors.blue,
+                          bufferedColor:
+                              const Color.fromRGBO(33, 150, 243, 0.2),
+                          backgroundColor: Colors.grey[800]!,
                         ),
                       ),
                     ),
-                  ),
                 ],
               ),
             ),
 
             // Video Controls
-            Positioned(
-              bottom: 16,
-              left: 0,
-              right: 0,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  IconButton(
-                    icon: Icon(
-                      _isPlaying ? Icons.pause : Icons.play_arrow,
-                      color: Colors.white,
-                      size: 32,
+            if (_isInitialized)
+              Positioned(
+                bottom: 16,
+                left: 0,
+                right: 0,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconButton(
+                      icon: Icon(
+                        _controller.value.isPlaying
+                            ? Icons.pause
+                            : Icons.play_arrow,
+                        color: Colors.white,
+                        size: 32,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          if (_controller.value.isPlaying) {
+                            _controller.pause();
+                          } else {
+                            _controller.play();
+                          }
+                        });
+                      },
                     ),
-                    onPressed: _togglePlayPause,
-                  ),
-                  const SizedBox(width: 16),
-                  IconButton(
-                    icon: const Icon(
-                      Icons.replay,
-                      color: Colors.white,
-                      size: 32,
+                    const SizedBox(width: 16),
+                    IconButton(
+                      icon: const Icon(
+                        Icons.replay,
+                        color: Colors.white,
+                        size: 32,
+                      ),
+                      onPressed: () {
+                        _controller.seekTo(Duration.zero);
+                        _controller.play();
+                      },
                     ),
-                    onPressed: _replay,
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
 
             // Next Button
             Positioned(
