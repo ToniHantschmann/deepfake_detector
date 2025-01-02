4e032d452d86227eb58d57d1bfb32908a5a043fd
hantschmann <anton.hantschmann@campus.lmu.de>
2025-01-02 13:11:21 +0100

    Revert "clean merge with selected files"
    This reverts commit 9f0785c1698a548fd59bb26fc165e393c254374a.
    
    got some wrong files from frontend

--- lib/blocs/game/game_event.dart
+++ lib/blocs/game/game_event.dart
@@ -1,7 +1,29 @@
 abstract class GameEvent {
-  //const GameEvent();
+  const GameEvent();
 }
 
-class GameState {
-  const GameState();
+class InitializeGame extends GameEvent {
+  const InitializeGame();
+}
+
+class LoginUser extends GameEvent {
+  final String username;
+  const LoginUser(this.username);
+}
+
+class StartGame extends GameEvent {
+  const StartGame();
+}
+
+class NextScreen extends GameEvent {
+  const NextScreen();
+}
+
+class SelectDeepfake extends GameEvent {
+  final int videoIndex;
+  const SelectDeepfake(this.videoIndex);
+}
+
+class RestartGame extends GameEvent {
+  const RestartGame();
 }
