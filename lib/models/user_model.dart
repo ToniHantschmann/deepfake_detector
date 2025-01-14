1e55e8df1806ede38de5286df859789741bb2072
hantschmann <anton.hantschmann@campus.lmu.de>
2025-01-14 18:11:50 +0100

    changed user model for pin implementation

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,11 +1,32 @@
 class User {
   final String username;
-  User({required this.username});
+  final String pin; // Neues Feld für PIN
+
+  User({
+    required this.username,
+    required this.pin,
+  });
 
   Map<String, dynamic> toJson() => {
         'username': username,
+        'pin': pin,
       };
+
   factory User.fromJson(Map<String, dynamic> json) {
-    return User(username: json['username']);
+    return User(
+      username: json['username'] as String,
+      pin: json['pin'] as String,
+    );
+  }
+
+  // Kopieren mit optionaler Änderung einzelner Felder
+  User copyWith({
+    String? username,
+    String? pin,
+  }) {
+    return User(
+      username: username ?? this.username,
+      pin: pin ?? this.pin,
+    );
   }
 }
